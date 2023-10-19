57cfc4ea271795f1b92fc6ecfb26d02c7a93a1ed
dlwjddn7922 <79899654+dlwjddn7922@users.noreply.github.com>
2023-10-19 23:47:21 +0900

    lee

---
+++ Assets/0.Script/Dice/DiceBlock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceBlock : MonoBehaviour
+{
+    public Dice dice = null;
+    public bool isDice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDice = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/0.Script/Dice/DiceSpawn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSpawn : MonoBehaviour
+{
+    [SerializeField] private Dice dice;
+    [SerializeField] private DiceBlock[] blocks;
+
+    int spawnCnt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    public void OnCreateDice()
+    {
+
+        if(true)
+        {
+            for(int i = 0; i < blocks.Length; i++) 
+            {
+                int rand = Random.Range(0, blocks.Length);
+                Vector3 createPos = blocks[rand].transform.position;
+                if (blocks[rand].isDice == false)
+                {
+                    blocks[rand].dice = Instantiate(dice, createPos, Quaternion.identity);
+                    blocks[rand].isDice = true;
+                    //blocks[rand].dice = blocks[i].dice;
+                    spawnCnt++;
+                    break;
+                }
+            }
+        }
+    }
+}
