382762e517f0b4412748c6dab491b1e473e15503
LeeJeongWoo <79899654+dlwjddn7922@users.noreply.github.com>
2023-10-19 19:49:42 +0900

    lee

--- Assets/0.Script/Enemy/Monster.cs
+++ Assets/0.Script/Enemy/Monster.cs
@@ -5,26 +5,33 @@ using DG.Tweening;
 
 public class Monster : MonoBehaviour
 {
-    [SerializeField] Transform[] wayPoints;
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
         DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
 
-        Vector3[] wayPointsVec = new Vector3[wayPoints.Length];
-        for (int i = 0; i < wayPoints.Length; i++)
-        {
-            wayPointsVec.SetValue(wayPoints[i].position, i);
-        }
-
-        transform.DOPath(wayPointsVec, 10f)
-            .SetEase(Ease.Linear)
-            .OnComplete(() => Destroy(gameObject));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void MonRoad(Transform[] road)
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        Vector3[] wayPointsVec = new Vector3[road.Length];
+        for (int i = 0; i < road.Length; i++)
+        {
+            wayPointsVec.SetValue(road[i].position, i);
+        }
+
+        transform.DOPath(wayPointsVec, 10f)
+            .SetEase(Ease.Linear)
+            .OnComplete(() => Destroy(gameObject));
+        sr.DOFade(1f, 1f);
     }
 }
--- Assets/0.Script/Enemy/SpawnController.cs
+++ Assets/0.Script/Enemy/SpawnController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class SpawnController : MonoBehaviour
 {
+    float timer;
+    float spawnTimer = 2f;
+    int maxSpawnCnt = 20;
+    int spawnCnt;
+    [SerializeField] private Monster monster;
+    [SerializeField] private Transform spawnPos;
+    [SerializeField] Transform[] wayPoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer > spawnTimer && spawnCnt < maxSpawnCnt)
+        {
+            Monster mon = Instantiate(monster);
+            mon.MonRoad(wayPoints);
+            mon.transform.SetParent(spawnPos);
+
+            timer = 0;
+            spawnCnt++;
+        }
     }
 }
