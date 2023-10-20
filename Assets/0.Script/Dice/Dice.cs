777afe9b74967796389877f724c9a4601def995b
dlwjddn7922 <79899654+dlwjddn7922@users.noreply.github.com>
2023-10-20 21:57:09 +0900

    lee

--- Assets/0.Script/Dice/Dice.cs
+++ Assets/0.Script/Dice/Dice.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,11 +10,14 @@ public class Dice : MonoBehaviour
     [HideInInspector] public DiceBlock diceBlock;
     [SerializeField] private Transform fireTrans;
     [SerializeField] private DiceBullet diceBullet;
+    [SerializeField] private List<GameObject> MonsterList = new List<GameObject>();
 
-    Monster mon;
+    public LayerMask layerMask;
+    public Transform etarget;
     Dice target;
     float fireTimer;
     float fireDelayTimer = 1f;
+
 
     public void OnMouseDown()
     {
@@ -22,7 +26,7 @@ public class Dice : MonoBehaviour
     }
     public void OnMouseUp()
     {
-        if(target)
+        if (target)
         {
             transform.position = target.transform.position;
             target.transform.position = startPos;
@@ -53,17 +57,79 @@ public class Dice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mon = GetComponent<Monster>();
         diceBlock = GetComponent<DiceBlock>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
     }
-    void FindCloseTarget()
+    Monster FindEnemy()
     {
+        Monster[] enemies = FindObjectsOfType<Monster>();
 
+        float minDis = float.MaxValue;
+        Monster m = null;
+        foreach (var enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (minDis >= distance)
+            {
+                minDis = distance;
+                m = enemy;
+            }
+        }
+        return m;
     }
+    void Attack()
+    {
+        Monster enemy = FindEnemy();
+        if (enemy == null)
+        {
+            return;
+        }
+
+        Vector3 vec = new Vector3(enemy.transform.position.x, enemy.transform.position.y);
+
+        fireTimer += Time.deltaTime;
+        if (fireTimer > fireDelayTimer)
+        {
+            fireTimer = 0f;
+
+            DiceBullet b = Instantiate(diceBullet, fireTrans);
+            b.SetTarget(enemy.transform);           
+            b.transform.SetParent(null);
+        }
+    }
+    void FindTarget()
+    {
+        for (int i = 0; i < MonsterList.Count; i++)
+        {
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(transform.position, MonsterList[i].transform.position - transform.position, out hit, 20f, layerMask);
+            float distance = float.MaxValue;
+            if (isHit && hit.transform.CompareTag("Monster"))
+            {
+                float dis = Vector2.Distance(transform.position, MonsterList[i].transform.position);
+                if (dis < distance)
+                {
+                    distance = dis;
+                    etarget = MonsterList[i].transform;
+                }
+            }
+        }
+    }
+/*    void Attack()
+    {
+        fireTimer += Time.deltaTime;
+        if (fireTimer > fireDelayTimer)
+        {
+            fireTimer = 0;
+            DiceBullet db = Instantiate(diceBullet, fireTrans);
+            //db.transform.Translate(MonsterList[0].transform.position);
+        }
+    }*/
 }
+
--- Assets/0.Script/Dice/DiceBullet.cs
+++ Assets/0.Script/Dice/DiceBullet.cs
@@ -6,16 +6,20 @@ public class DiceBullet : MonoBehaviour
 {
     float speed = 3f;
     [SerializeField] Transform target;
+    Dice dice;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dice = GetComponent<Dice>();
     }
 
     // Update is called once per frame
     void Update()
+    {        
+    }
+    public void SetTarget(Transform target)
     {
-        transform.Translate(target.position * Time.deltaTime * speed);
-
+        this.target = target;
+        //Vector3 vec = new Vector3(target.transform.position.x, target.transform.position.y) * speed;
     }
 }
